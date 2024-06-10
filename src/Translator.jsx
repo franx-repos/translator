c0fd288fe74d902dc59ebd1310ffe66b807e18b9
Frank Jäkel-Försterling <jaekel.frank@gmx.de>
2024-06-10 13:36:58 +0200

    data

--- src/Translator.jsx
+++ src/Translator.jsx
@@ -1,26 +1,30 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 
 const Translator = () => {
-
-  const [definition, setDefinition] = useState('');
-  const [searchTerm, setSearchTerm] = useState('');
+  const [definition, setDefinition] = useState({});
+  const [searchTerm, setSearchTerm] = useState("");
 
   const fetchDefinition = async (word) => {
-    const apiKey = 'dict.1.1.20240610T090637Z.f6d6efe832849511.e996d69148c86f9d5c0b20bb029c2989646ee383';
+    const apiKey =
+      "dict.1.1.20240610T090637Z.f6d6efe832849511.e996d69148c86f9d5c0b20bb029c2989646ee383";
     try {
-      const response = await axios.get(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${apiKey}&lang=en-en&text=${word}`);
-  
+      const response = await axios.get(
+        `https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${apiKey}&lang=en-en&text=${word}`
+      );
+
       if (response.status === 200) {
         const data = response.data;
-        const definition = data.def[0]?.tr[0]?.text || 'No definition found.';
+        // const definition = data.def[0]?.tr[0]?.text || 'No definition found.';
+        const definition = data.def[0] || "No definition found.";
         setDefinition(definition);
+        console.log(definition);
       } else {
-        setDefinition('No definition found.');
+        setDefinition("No definition found.");
       }
     } catch (error) {
-      console.error('Error fetching definition:', error);
-      setDefinition('No definition found.');
+      console.error("Error fetching definition:", error);
+      setDefinition("No definition found.");
     }
   };
 
@@ -29,8 +33,8 @@ const Translator = () => {
     try {
       await fetchDefinition(searchTerm);
     } catch (error) {
-      console.error('Error fetching definition:', error);
-      setDefinition('No definition found.');
+      console.error("Error fetching definition:", error);
+      setDefinition("No definition found.");
     }
   };
 
@@ -47,8 +51,20 @@ const Translator = () => {
         <button onClick={handleSearch}>Search</button>
       </div>
       <div>
-        <h2>Definition:</h2>
-        <p>{definition}</p>
+        {definition.tr ? (
+          <>
+            <p>word type: {definition.pos}</p>
+            <p>phonetic spelling: {definition.ts}</p>
+            <ul>
+              <p>synonyms:</p>
+              {definition.tr?.map((entry) => {
+                return <li>{`${entry.text} > ${entry.ts}`}</li>;
+              })}
+            </ul>
+          </>
+        ) : (
+          <p>nothing found</p>
+        )}
       </div>
     </div>
   );
