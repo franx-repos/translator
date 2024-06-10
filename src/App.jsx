e7129c1a1ab7f27fcefc48a7af0c76964bca4717
Muhammad-N-Ali <m.naeemali572@gmail.com>
2024-06-10 13:13:01 +0200

    html tages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Translator from "./Translator";
 function App() {
   return (
     <div className="App">
+
       <Translator />
     </div>
   );
--- src/Translator.jsx
+++ src/Translator.jsx
@@ -4,7 +4,31 @@ import useFetchData from "./UseFetchData";
 function Translator() {
   return (
     <>
+
       <h1>Translator</h1>
+
+      <header>
+        <h1>Simple Dictionary</h1>
+        <p>make your vocabulary strong and know more about the World of Words</p>
+      </header>
+
+      <div>
+        <input type="text" />
+        <button>Click</button>
+      </div>
+
+      <div>
+        <h3>Meanings & Definition:</h3>
+        <h3>Examples:</h3>
+        <h3>Noun:</h3>
+        <h3>Verb:</h3>
+        <h3>Adjective:</h3>
+      </div>
+
+      <footer>
+        <p>copy write reserved @ xyz.dictionary 2024</p>
+      </footer>
+
     </>
   );
 }
